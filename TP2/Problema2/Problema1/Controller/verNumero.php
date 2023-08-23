9185abe71b1832e0e2a8536ccc7b3302a20609fe
acostaDemianAaron <acostademiann14@gmail.com>
2023-08-22 21:24:38 -0300

    TP2-Pro2-Problema1 Terminado...

--- TP2/Problema2/Problema1/Controller/verNumero.php
+++ TP2/Problema2/Problema1/Controller/verNumero.php
@@ -1,23 +1,52 @@
 <?php
-echo '<link href="../View/css/bootstrap5.3.1.min.css" rel="stylesheet">';
+echo '<link rel="stylesheet" href="../View/css/bootstrap5.3.1.min.css">';
+echo '<link rel="stylesheet" href="../View/css/estilos.css">';
 
-echo '<div class="container p-5">';
+
+
+
+echo '<header style="height: 70px; background: linear-gradient(to right, #2B32B2, #1488CC);"></header>';
+
+echo '
+<main>
+<div style="height: 30px;"></div>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card shadow-lg p-3 mb-5 bg-white ">
+            <div class="card-header">Formulario de prueba</div><br/>
+                <div class="card-body">';
 
 if ($_GET) {
 
     $num = $_GET['num'];
     switch ($num) {
         case 0:
-            echo "<h1><strong>El número " . $num . " es cero</strong></h1>";
+            echo "<h3><strong>El número " . $num . " es cero</strong></h3>";
             break;
         case $num > 0:
-            echo "<h1><strong>El número " . $num . " es positivo</strong></h1>";
+            echo "<h3><strong>El número " . $num . " es positivo</strong></h3>";
             break;
         case $num < 0:
-            echo "<h1><strong>El número " . $num . " es negativo</strong></h1>";
+            echo "<h3><strong>El número " . $num . " es negativo</strong></h3>";
             break;
     }
 } else {
-    echo '<h1 style="color: red">No se ingreso ningun valor.</h1>';
+    echo '<h3 style="color: red">No se ingreso ningun valor.</h3>';
 }
+
+echo '
+    <br/>
+    <form action="../View/html/verNumeroIndex.php">
+        <button class="btn btn-primary" type="submit" name="submit">Volver</button>
+    </form>
+    </div>';
 echo '</div>';
+
+echo '
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</main>';
--- TP2/Problema2/Problema1/View/html/verNumeroIndex.php
+++ TP2/Problema2/Problema1/View/html/verNumeroIndex.php
@@ -5,26 +5,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/bootstrap5.3.1.min.css">
-    <link rel="stylesheet" href="../css/estilo.css">
+    <link rel="stylesheet" href="../css/estilos.css">
     <title>Problema1</title>
 </head>
 
 <body>
-    <div class="container p-2">
-        <h1>Número positivo, negativo o cero</h1>
-        <div class="row g-3 align-items-center">
-            <form action="../../Controller/verNumero.php" method="GET">
-                <!--Comprueba si el número ingresado es positivo, negativo o cero-->
-                <div class="col-auto">
-                    <label class="col-form-label">Ingrese un número: </label>
-                    <input name="num" id="num" type="number" required>
+    <main>
+        <header style="height: 70px">
+        </header>
+        <div style="height: 30px;"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card shadow-lg p-3 mb-5 bg-white ">
+                        <div class="card-header">Formulario de prueba</div>
+                        <div class="card-body">
+                            <form action="../../Controller/verNumero.php" method="GET" class="needs-validation" novalidate>
+                                <div class="form-row">
+                                    <div class="col-md-4 mb-3">
+                                        <label for="num">Ingrese un número: </label>
+                                        <input id="num" name="num" type="number" class="form-control" placeholder="Ej: -1, 0, 1" value="" required>
+                                        <div class="valid-feedback">¡Ok válido!</div>
+                                        <div class="invalid-feedback">Complete el campo.</div>
+                                    </div>
+                                </div>
+                                <button class="btn btn-primary" type="submit">Enviar</button>
+                            </form>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-auto pt-2">
-                    <button name="enviar" id="enviar" type="submit" class="btn btn-light">Enviar</button>
-                </div>
-            </form>
+            </div>
         </div>
-    </div>
+    </main>
+    <script src="../js/campoVacio.js"></script>
 </body>
 
 </html>
