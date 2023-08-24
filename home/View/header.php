8d3de8b22f8d97add1cb8477b4e58ead93ea3a6c
veraAlan <vera.alan2207@gmail.com>
2023-08-24 20:37:00 -0300

    Changed menu and libs

---
+++ home/View/header.php
@@ -0,0 +1,19 @@
+<head>
+    <!-- Libraries -->
+    <link rel="stylesheet" href="/home/libs/Bootstrap5.3.1/css/bootstrap.min.css">
+    <script src="/home/libs/Bootstrap5.3.1/js/bootstrap.bundle.min.js"></script>
+    <script src="/home/libs/Bootstrap5.3.1/js/bootstrap.min.js"></script>
+    <script src="/home/libs/jQuery/jQuery-3.7.0.js"></script>
+</head>
+
+<div class="container">
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+            <span class="fs-4">TPs Menu</span>
+        </a>
+
+        <ul class="nav nav-pills">
+            <li class="nav-item"><a href="/" class="nav-link active" aria-current="page">Home</a></li>
+        </ul>
+    </header>
+</div>
