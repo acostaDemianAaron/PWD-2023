6316cb0ccbd3547cb59856d5cde9742f39bf8a6f
acostaDemianAaron <acostademiann14@gmail.com>
2023-08-25 20:05:46 -0300

    Listo

--- TP3/Problema3/View/html/aCinemas.php
+++ TP3/Problema3/View/html/aCinemas.php
@@ -9,7 +9,7 @@ include_once('../../Controller/UploadController.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Cinem@s</title>
 </head>
 
 <body>
