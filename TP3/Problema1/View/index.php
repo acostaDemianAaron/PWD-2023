e4aec6364a087bfaa8ed52487a6cd44dbb8a2463
acostaDemianAaron <acostademiann14@gmail.com>
2023-08-25 14:10:52 -0300

    quitamos CDN

--- TP3/Problema1/View/index.php
+++ TP3/Problema1/View/index.php
@@ -7,7 +7,6 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Subir Archivo al Servidor</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
@@ -23,7 +22,6 @@
     </form>
   </div>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
