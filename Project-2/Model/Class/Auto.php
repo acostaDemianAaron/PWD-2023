ffc6df63718cc0d6ffeef8e82fd442d0baef0f88
acostaDemianAaron <acostademiann14@gmail.com>
2023-09-11 22:56:30 -0300

    Se agrego la clase Auto

---
+++ Project-2/Model/Class/Auto.php
@@ -0,0 +1,17 @@
+<?php
+
+class Auto
+{
+    private $patente;
+    private $marca;
+    private $modelo;
+    private $dniDuenio;
+
+    public function __construct()
+    {
+        $this->patente;
+        $this->marca;
+        $this->modelo;
+        $this->dniDuenio;
+    }
+}
