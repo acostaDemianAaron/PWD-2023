b6b160ab1fabc62aedec81c40431c6ecfaeb8815
acostaDemianAaron <acostademiann14@gmail.com>
2023-09-12 21:58:17 -0300

    ...

--- Project-2/View/Action/nuevaPersona.php
+++ Project-2/View/Action/nuevaPersona.php
@@ -1,8 +1,9 @@
 <?php
 require_once("../../Config/configuracion.php");
-require_once("../../View/Action/nuevaPersona.php");
+require_once("../../View/Structure/header.php");
 $persona = new AbmPersona;
 $data = data_submitted();
+
 ?>
 
 
@@ -11,15 +12,32 @@ $data = data_submitted();
 </head>
 
 <body>
-    <?php
-    if ($persona->Search($data['NroDni']) == null) {
-        if ($persona->Add($data)) {
-            echo "Se cargo la persona";
-        } else {
-            echo "No se pudo cargar el dato en la base de datos";
-        }
-    } else {
-        echo "Ya existe una persona con ese DNI";
-    }
-    ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card-body">
+                    <div class="card">
+                        <div class="card-header">
+                            <h6 class="m-2">Respuesta del servidor</h6>
+                        </div>
+                        <div class="card-body">
+                            <blockquote class="blockquote mb-0">
+                                <?php
+                                if ($persona->Search($data['NroDni']) == null) {
+                                    if ($persona->Add($data)) {
+                                        echo '<h6 class="m-4">Se cargo la persona</h6>';
+                                    } else {
+                                        echo '<h6 class="m-4">No se pudo cargar el dato en la base de datos</h6>';
+                                    }
+                                } else {
+                                    echo '<h6 class="m-4">Ya existe una persona con ese DNI</h6>';
+                                }
+                                ?>
+                            </blockquote>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
