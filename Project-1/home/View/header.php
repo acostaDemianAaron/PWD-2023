d7aecc365aa347245b9e8971eb483c1d5b1f9e60
Cristian Vera <vera.alan2207@gmail.com>
2023-10-18 21:23:53 -0300

    Small changes

--- Project-1/home/View/header.php
+++ Project-1/home/View/header.php
@@ -1,21 +1,21 @@
 <head>
     <!-- Libraries -->
-    <link rel="stylesheet" href="/home/libs/Bootstrap5.3.1/css/bootstrap.min.css">
-    <script src="/home/libs/Bootstrap5.3.1/js/bootstrap.bundle.min.js"></script>
-    <script src="/home/libs/Bootstrap5.3.1/js/bootstrap.min.js"></script>
-    <script src="/home/libs/jQuery/jquery-3.7.0.min.js"></script>
-    <script src="/home/libs/jQuery/jquery.validate.min.js"></script>
-    <script src="/home/libs/jQuery/translate-jQuery.js"></script>
+    <link rel="stylesheet" href="../../home/libs/Bootstrap5.3.1/css/bootstrap.min.css">
+    <script src="../../home/libs/Bootstrap5.3.1/js/bootstrap.bundle.min.js"></script>
+    <script src="../../home/libs/Bootstrap5.3.1/js/bootstrap.min.js"></script>
+    <script src="../../home/libs/jQuery/jquery-3.7.0.min.js"></script>
+    <script src="../../home/libs/jQuery/jquery.validate.min.js"></script>
+    <script src="../../home/libs/jQuery/translate-jQuery.js"></script>
 </head>
 
 <div class="container">
     <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+        <a href="/PWD-2023/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
             <span class="fs-4">TPs Menu</span>
         </a>
 
         <ul class="nav nav-pills">
-            <li class="nav-item"><a href="/" class="nav-link active" aria-current="page">Home</a></li>
+            <li class="nav-item"><a href="/PWD-2023/Project-1/home/View/menu.php" class="nav-link active" aria-current="page">Home</a></li>
         </ul>
     </header>
 </div>
